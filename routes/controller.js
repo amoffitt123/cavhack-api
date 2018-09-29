459b48536c62a60d2c6d358ebf01588da771772a
Moffitt <1767783@fcps.edu>
2018-09-29 17:45:41 -0400

    my project

--- routes/controller.js
+++ routes/controller.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var router = express.Router();
-var Profile = require('../models').profile
-//var Profile = require('../mock/profile')
+//var Profile = require('../models').profile
+var Profile = require('../mock/profile')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.sendStatus(200)
+  res.send('hello world')
 });
+router.get('/profiles', function (req, res) {
+  res.send(Profile.findAll())
+})
+router.delete('/profiles/:id', function (req, res) {
+  var profiles = Profile.remove(req.params.id)
+  res.send(profiles)
+})
+router.put('/profiles/:id', function (req, res) {
+  var profile = Profile.update(req.params.id, {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    bio: req.body.bio,
+    interests: req.body.interests,
+    skills: req.body.skills
+  })
+  res.send(profile)
+})
 
- 
+
 module.exports = router;
